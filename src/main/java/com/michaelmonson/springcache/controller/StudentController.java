f7956bc261c48710626bf25218d049d9fca02f81
Michael Monson <michaelmonson@gmail.com>
2023-09-11 09:00:33 -0600

    Added additional methods to Student Controller and Service.   - Upgraded Swagger SpringDoc version   - Added additional Swagger annotations to existing methods.

--- src/main/java/com/michaelmonson/springcache/controller/StudentController.java
+++ src/main/java/com/michaelmonson/springcache/controller/StudentController.java
@@ -1,23 +1,52 @@
 
 package com.michaelmonson.springcache.controller;
 
+import com.michaelmonson.springcache.domain.Student;
+import com.michaelmonson.springcache.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.michaelmonson.springcache.domain.Student;
-import com.michaelmonson.springcache.service.StudentService;
 
 import java.util.List;
 
+@Tag(name = "Student Controller", description = "Allows Students to be added, updated, and removed from the system.  Currently the system does not persist student data permanently.")
 @RestController
+@RequestMapping("/student")
 //@Api(tags = "Student Controller")
 public class StudentController {
 
     @Autowired
     StudentService studentService;
+
+
+    /**
+     * Add a student to our school.
+     *
+     * @param student represents a specific student with contact information
+     * @return student object with a generated ID
+     */
+    @Operation(summary = "Add Student Record", description = "Add a new student and associated information.  A new ID will be added to the student record.")
+    @PostMapping("")
+    public Student addStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
+
+    @Operation(summary = "Update Student by ID", description = "Update the specified student.")
+    @PutMapping("/{id}")
+    public Student updateStudent(
+            @PathVariable Long id,
+            @RequestBody Student student) {
+
+        return studentService.updateStudent(student, id);
+    }
 
     /**
      * Get back a student by the specified student ID.
@@ -25,14 +54,16 @@ public class StudentController {
      * @param id Student ID
      * @return the student specified by the ID
      */
-    @GetMapping("/student/{id}")
+    @Operation(summary = "Get Student by ID", description = "Return the student data by the specified ID.  Contact information is also returned.")
+    @GetMapping("/{id}")
     public Student findStudentById(@PathVariable Long id) {
         System.out.println("Searching by ID  : " + id);
 
         return studentService.getStudentByID(id);
     }
 
-    @DeleteMapping("/student/{id}")
+    @Operation(summary = "Delete Student by ID", description = "Delete a student by the specified student ID.")
+    @DeleteMapping("/{id}")
     public void deleteStudent(@PathVariable Long id) {
         System.out.println("Deleting Student Entity for ID: " + id);
         studentService.deleteStudent(id);
@@ -43,7 +74,8 @@ public class StudentController {
      *
      *  @return List of Students currently residing in memory cache
      */
-    @GetMapping("/student")
+    @Operation(summary = "Return a list of all Students", description = "Returns all students stored in our cache.")
+    @GetMapping("")
     public List<Student> getAllStudents() {
         return studentService.getStudents();
     }
--- src/main/java/com/michaelmonson/springcache/service/StudentService.java
+++ src/main/java/com/michaelmonson/springcache/service/StudentService.java
@@ -11,6 +11,7 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 @Service
@@ -25,30 +26,54 @@ public class StudentService {
 
 	@Cacheable("student")
 	public Student getStudentByID(Long studentId) {
-    try {
-    	System.out.println("  --> Going to sleep for 2 Secs.. to simulate backend call.");
-      	Thread.sleep(1000 * 2);
+		try {
+			System.out.println("  --> Going to sleep for 2 Secs.. to simulate backend call.");
+			Thread.sleep(1000 * 2);
 
-    } catch (InterruptedException e) {
-		e.printStackTrace();
-    }
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
-    // Provides utility methods for generating fake strings, such as names and numbers
-    Faker faker = new Faker();
-    Name studentName = faker.name();
-    Address address = faker.address();
-    String nameFirst = address.firstName();
-	String nameLast = address.lastName();
-	String nameMiddle = new Faker().name().firstName(); //although the 'fullName' will often return a middle name, there isn't a method to grab the middle name separately.
-	String fullName = nameFirst + " " + nameMiddle + " " + nameLast;
-    String activeStudent =  Math.random() > 0.5 ? "Y" : "N";
+		// Provides utility methods for generating fake strings, such as names and numbers
+		Faker faker = new Faker();
+		Name studentName = faker.name();
+		Address address = faker.address();
+		String nameFirst = address.firstName();
+		String nameLast = address.lastName();
+		String nameMiddle = new Faker().name().firstName(); //although the 'fullName' will often return a middle name, there isn't a method to grab the middle name separately.
+		String fullName = nameFirst + " " + nameMiddle + " " + nameLast;
+		String activeStudent =  Math.random() > 0.5 ? "Y" : "N";
 
-    ContactInfo contactInfo = new ContactInfo(UUID.randomUUID().toString(), studentId,
-            address.streetAddress(), address.city(), address.stateAbbr(), address.zipCode(),
-            faker.phoneNumber().phoneNumber(), faker.internet().emailAddress());
+		ContactInfo contactInfo = new ContactInfo(UUID.randomUUID().toString(), studentId,
+				address.streetAddress(), address.city(), address.stateAbbr(), address.zipCode(),
+				faker.phoneNumber().phoneNumber(), faker.internet().emailAddress());
 
-    return new Student(studentId, fullName, nameLast, nameFirst, nameMiddle,
-		studentName.title(), activeStudent, contactInfo);
+		return new Student(studentId, fullName, nameLast, nameFirst, nameMiddle,
+			studentName.title(), activeStudent, contactInfo);
+	}
+
+
+	@Cacheable("student")
+	public Student addStudent(Student student) {
+
+		//Generate Student ID:
+		Random random = new Random();
+		student.setStudentId(random.nextLong());
+		student.getContactInfo().setStudentId(student.getStudentId());
+
+		//Not really saving to a repository yet... just writing to cache what they sent us, with an ID added...
+		return student;
+	}
+
+	@Cacheable("student")
+	public Student updateStudent(Student student, Long studentId) {
+
+		student.setStudentId(studentId);
+		student.getContactInfo().setStudentId(studentId);
+
+		//Add business logic and validation:
+
+		return student;
 	}
 
 
@@ -58,7 +83,11 @@ public class StudentService {
 		System.out.println("  --> Student #" + studentId + " has been deleted from memory cache.");
 	}
 
-
+	/**
+	 * This endpoint returns a list of all students contained in cache.
+	 *
+	 * @return List of Students
+	 */
 	public List<Student> getStudents() {
 
     	List<Object> cacheObjects = cacheService.getAllEntriesFromStudentCache("student");
